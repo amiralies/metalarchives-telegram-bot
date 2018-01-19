d39e0f99ab9cf87cb13fd681151d3029d4e1f7b6
Amirali Esmaeili <esmailiamirali@gmail.com>
2018-01-19 22:41:28 +0330

    Update message handler switch/case to if else style

--- src/handlers/messages/text.js
+++ src/handlers/messages/text.js
@@ -11,23 +11,21 @@ const textHandler = (ctx) => {
     message,
   } = ctx;
 
-  switch (session.chatState) {
-    case chatStates.AWAITING_BAND_QUERY:
-      session.chatState = chatStates.AWAITING_COMMAND;
-      return searchBand(message.text, 0).then((res) => {
-        const { msgText, msgKeyboard } = genBandResult(res, ctx);
-        return replyWithMarkdown(msgText, msgKeyboard.extra());
-      }).then((res) => {
-        session.searchQuery = message.text;
-        session.searchResultMsgId = res.message_id;
-      }).catch((err) => {
-        if (err.response && err.response.status === 404) {
-          return reply((i18n.t('not_found')));
-        }
-        console.error(err);
-        return reply(i18n.t('error'));
-      });
-    default:
+  if (session.chatState === chatStates.AWAITING_BAND_QUERY) {
+    session.chatState = chatStates.AWAITING_COMMAND;
+    return searchBand(message.text, 0).then((res) => {
+      const { msgText, msgKeyboard } = genBandResult(res, ctx);
+      return replyWithMarkdown(msgText, msgKeyboard.extra());
+    }).then((res) => {
+      session.searchQuery = message.text;
+      session.searchResultMsgId = res.message_id;
+    }).catch((err) => {
+      if (err.response && err.response.status === 404) {
+        return reply((i18n.t('not_found')));
+      }
+      console.error(err);
+      return reply(i18n.t('error'));
+    });
   }
 };
 
